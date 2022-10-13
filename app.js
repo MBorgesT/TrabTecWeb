24248aa153efcaa3aab33522a240e0d7a41a5783
Matheus Borges <matheus.borgest@gmail.com>
2022-10-13 19:18:00 +0100

    correções e melhoramentos

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-import { Game } from './game.js';
+function fillBoard() {
+    let nRows = 10;
+    let nCols = 5;
 
+    let board = [3, 10, 6, 2, 8];
+    let boardElem = document.getElementById('board');
 
-let game = new Game(7, 3);
-game.board = [6, 5, 4];
+    for (let i = 0; i < nCols; i++) {
+        let colElem = document.createElement('span');
+        for (let j = 0; j < board[i]; j++) {
+            let circleWrap = document.createElement('div');
+            circleWrap.className = 'circleWrap';
+            circleWrap.style.width = `50px`;
+            circleWrap.style.height = `50px`;
 
+            let circle = document.createElement('div');
+            circle.className = 'circle';
+            circle.style.width = '100%';
+            circle.style.height = '100%';
+            circleWrap.appendChild(circle);
 
-console.log(game.board)
-game.printBoard();
-
-let matDecomp = game.binaryDecomposition();
-for (let i = 0; i < matDecomp.length; i++) {
-    console.log(matDecomp[i].reverse());
+            colElem.appendChild(circleWrap);
+        }
+        boardElem.appendChild(colElem);
+    }
 }
 
-console.log('\n\n');
-
-
-game.doBestPlay();
-
-console.log(game.board)
-game.printBoard();
-
-matDecomp = game.binaryDecomposition();
-for (let i = 0; i < matDecomp.length; i++) {
-    console.log(matDecomp[i].reverse());
-}
+window.onload = () => fillBoard();
--- game.js
+++ game.js
@@ -4,6 +4,9 @@ export class Game {
     constructor(_nUIRows, _nUICols, _difficulty) {
         // os números de rows e cols aqui são referentes à como é exibido na tela
         // por motivos de praticidade, esses valores são invertidos na modelagem
+        this.nUIRows = _nUIRows;
+        this.nUICols = _nUICols;
+
         this.nRows = _nUICols;
         this.nCols = _nUIRows;
 
@@ -118,6 +121,17 @@ export class Game {
         return true;
     }
 
+    
+    canEndInNextMove() {
+        let count = 0;
+        for (let i = 0; i < this.nRows; i++) {
+            if (this.board[i] > 0) {
+                count++;
+            }
+        }
+        return (count == 1);
+    }
+
 
     getFirstUnbalancedRow() {
         let matDecomp = this.binaryDecomposition();
@@ -145,16 +159,24 @@ export class Game {
         let row = Math.floor(Math.random() * possibleMoves.length);
 
         // quantidade aleatória
-        if (amountToRemove == -1) {
+        if (amountToRemove == null) {
             amountToRemove = Math.floor(Math.random() * (this.board[row] - 1)) + 1;
         }
 
-        this.play(possibleMoves[col], amountToRemove);
+        this.play(possibleMoves[row], amountToRemove);
     }
 
 
     doBestMove() {
-        if (this.isBalanced()) {
+        if (this.canEndInNextMove()) {
+            for (let i = 0; i < this.nRows; i++) {
+                if (this.board[i] > 0) {
+                    this.play(i, this.board[i]);
+                    break;
+                }
+            }
+        }
+        else if (this.isBalanced()) {
             // play random move
             this.doRandomMove(1)
         } else {
@@ -180,10 +202,10 @@ export class Game {
 
 
     doAIMove() {
-        if (Math.floor(Math.random() * 10) > this.difficulty) {
+        if (Math.floor(Math.random() * 10) < this.difficulty) {
             this.doBestMove();
         } else {
-            this.doRandomMove(-1);
+            this.doRandomMove();
         }
     }
 
